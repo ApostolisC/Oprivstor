81a7e70847bfd22e3ae84cce8f40a195fd245bca
Apostolos Chasiotis <67831730+ApostolisC@users.noreply.github.com>
2023-04-14 13:21:52 +0000

    Update server.py

--- server/server.py
+++ server/server.py
@@ -17,9 +17,6 @@ from personalDatabase import personalDatabase
 
 from handler import Handler
 
-"""
-* encrypt personal database
-"""
 
 class Server:
     window=None
@@ -32,11 +29,11 @@ class Server:
     def printDB(self):
         self.H.printDatabase()
 
-    def sendMessage(self, client, message, client_public_key=None, signature_public_key=None):
+    def sendMessage(self, client, message, client_public_key=None):
         if client_public_key:
-            message = self.cr.createMessage(message,client_public_key, self.private_key, signature_public_key)
+            message = self.cr.createMessage(message,client_public_key, self.private_key)
             size_int=len(message)
-            size = self.cr.createMessage(str(size_int).encode(), client_public_key, self.private_key,signature_public_key)
+            size = self.cr.createMessage(str(size_int).encode(), client_public_key, self.private_key)
         else:
             size = str(len(message)).encode()
             size_int=len(message)
@@ -66,13 +63,13 @@ class Server:
         else:
             return True
 
-    def sendFile(self, client, filename, compress, nonce, client_public_key, signature_public_key):
+    def sendFile(self, client, filename, compress, nonce, client_public_key):
         with open(filename, "rb") as f:
             try:
 
                 size = os.path.getsize(filename)
                 filename = os.path.basename(filename).encode().hex()
-                self.sendMessage(client,("%s %s %s %s"%(filename, str(size), compress, nonce)).encode(), client_public_key,signature_public_key)
+                self.sendMessage(client,("%s %s %s %s"%(filename, str(size), compress, nonce)).encode(), client_public_key)
 
                 wait = client.recv(1024)
 
@@ -105,28 +102,29 @@ class Server:
             self.sendMessage(client, ("Hallo from Server %s"%uuid_).encode(),client_public_key)
 
         else:
-            client.send(self.cr.createMessage(b"0", client_public_key, self.private_key,client_public_key))
+            client.send(self.cr.createMessage(b"0", client_public_key, self.private_key))
         ##########################################
-            data = self.cr.decryptMessage(client.recv(1024), self.private_key).decode().split()
+            data = self.cr.decryptMessage(client.recv(1024), self.private_key, client_public_key).decode().split()
             command_uuid = data[0]
             username = data[1]
             command = data[2:]
 
-            userID, signature_public_key = self.H.getUserID(username, with_public_key=True)
+            userID, client_perm_public_key = self.H.getUserID(username, with_public_key=True)
+            client_perm_public_key = self.cr.getPublicFromPEM(client_perm_public_key)
 
             user_uuid = self.H.getUUID(username)
 
 
             if user_uuid==command_uuid:
                 if command[0] not in ("MKDIR", "DELETE", "RENAME"):
-                    client.send(self.cr.createMessage(b"0", client_public_key, self.private_key, signature_public_key))
+                    client.send(self.cr.createMessage(b"0", client_public_key, self.private_key))
             else:
-                client.send(self.cr.createMessage(b"1", client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"1", client_public_key, self.private_key))
                 client.close()
                 return
 
 
-            self.executeCommand(userID, command_uuid,client, client_public_key,signature_public_key, command)
+            self.executeCommand(userID, command_uuid,client, client_public_key,client_perm_public_key, command)
             return
 
 
@@ -146,34 +144,34 @@ class Server:
 
             action = bytes.fromhex(command[0]).decode()
             if action not in ("LOGIN", "SIGNUP") or len(command)!=3:
-                self.sendMessage(client, b" Invalid Command. Action reported!",client_public_key,signature_public_key)
+                self.sendMessage(client, b" Invalid Command. Action reported!",client_public_key)
                 client.close()
                 return
             username = bytes.fromhex(command[1]).decode()
             password = bytes.fromhex(command[2]).decode()
             if not username:
-                self.sendMessage(client, b"1Username must not be null.", client_public_key,signature_public_key)
+                self.sendMessage(client, b"1Username must not be null.", client_public_key)
                 continue
             if " " in username:
-                self.sendMessage(client, b"1Username must not contain spaces.", client_public_key,signature_public_key)
+                self.sendMessage(client, b"1Username must not contain spaces.", client_public_key)
                 continue
 
 
             if action=="SIGNUP":
                 if len(password) < 8:
-                    self.sendMessage(client,b"1Password must be at least of length 8!", client_public_key,signature_public_key)
+                    self.sendMessage(client,b"1Password must be at least of length 8!", client_public_key)
                     continue
                 elif len(password) >255:
-                    self.sendMessage(client,b"1Password is too big! Maximum length: 255.", client_public_key,signature_public_key)
+                    self.sendMessage(client,b"1Password is too big! Maximum length: 255.", client_public_key)
                     continue
                 elif len(username) > 32:
-                    self.sendMessage(client,b"1Username is too big! Maximum length: 32.", client_public_key,signature_public_key)
+                    self.sendMessage(client,b"1Username is too big! Maximum length: 32.", client_public_key)
                     continue
                 if self.H.userExists(username):
-                    self.sendMessage(client, b"1User already exists!", client_public_key,signature_public_key)
+                    self.sendMessage(client, b"1User already exists!", client_public_key)
                 else:
-                    self.sendMessage(client, b"0",client_public_key,signature_public_key)
-                    enc_info = self.cr.decryptMessage(client.recv(1024*1024), self.private_key).decode()
+                    self.sendMessage(client, b"0",client_public_key)
+                    enc_info = self.cr.decryptMessage(client.recv(1024*1024), self.private_key, client_public_key).decode()
 
                     #salt = enc_info[0:16]
                     #encrypted_master_passwd = enc_info[16:48]
@@ -183,20 +181,23 @@ class Server:
                     salt, encrypted_master_passwd, iv, tag, hash, public_key, encrypted_private_key, nonce = [bytes.fromhex(v) for v in enc_info.splitlines()]
                     info = [hash, salt,encrypted_master_passwd,iv,tag, public_key, encrypted_private_key, nonce]
                     userID = self.H.createUser(username,password, info)
-                    self.sendMessage(client, b"0",client_public_key,signature_public_key)
+                    self.sendMessage(client, b"0",client_public_key)
+
+
+                    client_perm_public_key = self.cr.getPublicFromPEM(public_key)
 
                     break
             else:
                 enc_info ,userID = self.H.getMasterPassword(username, password)
                 if (enc_info,userID)==(None,None):
-                    self.sendMessage(client,b"1Wrong Username or Password. Please Try Again.", client_public_key,signature_public_key)
+                    self.sendMessage(client,b"1Wrong Username or Password. Please Try Again.", client_public_key)
 
                 else:
-                    signature_public_key = enc_info[1]
-                    self.sendMessage(client,b"0[Server]: Welcome %b!"%username.encode(),client_public_key,signature_public_key)
+                    client_perm_public_key = self.cr.getPublicFromPEM(enc_info[1])
+                    self.sendMessage(client,b"0[Server]: Welcome %b!"%username.encode(),client_public_key)
                     wait = client.recv(1024)
                     message = "%s\n%s\n%s\n\n%s"%(enc_info[0].hex(),enc_info[1].hex(),enc_info[2].hex(),enc_info[3].hex())
-                    self.sendMessage(client, message.encode(), client_public_key,signature_public_key)
+                    self.sendMessage(client, message.encode(), client_public_key)
                     break
 
         #If user managed to log into his account, the loop will break knowing we can execute commands
@@ -213,10 +214,10 @@ class Server:
                 if not command:
                     client.close()
                     return
-                command=self.cr.decryptMessage(command,self.private_key).decode().split()
+                command=self.cr.decryptMessage(command,self.private_key, client_perm_public_key).decode().split()
 
                 if len(command)!=2 or command[0]!="LS":
-                    self.sendMessage(client, b"1Invalid command", client_public_key,signature_public_key)
+                    self.sendMessage(client, b"1Invalid command", client_public_key)
                     client.close()
                     return
 
@@ -225,7 +226,7 @@ class Server:
                 files = personal_database.getFiles(path)
 
                 if not files:
-                    self.sendMessage(client, b"", client_public_key,signature_public_key)
+                    self.sendMessage(client, b"", client_public_key)
                 else:
                     buffer=b""
                     for file in files:
@@ -235,15 +236,15 @@ class Server:
 
                         buffer+=("%s %s %s %s %s %s "%(filename, file_type, size,modification_date, upload_date, compressed)).encode()
                         if len(buffer) > 200*1024:
-                            self.sendMessage(client,buffer, client_public_key,signature_public_key)
+                            self.sendMessage(client,buffer, client_public_key)
                             buffer=b""
-                    self.sendMessage(client, buffer, client_public_key,signature_public_key) if buffer else self.sendMessage(client, b"", client_public_key,signature_public_key)
+                    self.sendMessage(client, buffer, client_public_key) if buffer else self.sendMessage(client, b"", client_public_key)
 
             except ConnectionResetError:
                 client.close()
                 return
 
-    def executeCommand(self,userID, command_uuid, client, client_public_key, signature_public_key, command):
+    def executeCommand(self,userID, command_uuid, client, client_public_key, client_perm_public_key, command):
         personal_database = personalDatabase(os.path.join("database","databases",userID+".db"))
         cpath = os.path.join("database","FILES",userID)
 
@@ -255,11 +256,11 @@ class Server:
 
             path_to_file = personal_database.getFile(dirname, basename)
             if not path_to_file:
-                self.sendMessage(client, b"1Invalid Path!", client_public_key,signature_public_key)
+                self.sendMessage(client, b"1Invalid Path!", client_public_key)
                 client.close()
                 return
             if path_to_file[3]=="DIR":
-                self.sendMessage(client, b"1Item is not a file", client_public_key,signature_public_key)
+                self.sendMessage(client, b"1Item is not a file", client_public_key)
                 client.close()
                 return
 
@@ -269,7 +270,7 @@ class Server:
             #file_info = self.H.getUserFile(userID, "/" if dirname==userID else dirname, os.path.basename(item_to_download))[0]
             compress, nonce = path_to_file[7:]
 
-            self.sendFile(client, os.path.join("database","FILES", userID, path_to_file[0]), compress, nonce, client_public_key,signature_public_key)
+            self.sendFile(client, os.path.join("database","FILES", userID, path_to_file[0]), compress, nonce, client_public_key)
 
             personal_database.updateModDate(dirname, os.path.basename(item_to_download), str(datetime.datetime.now()).split(".")[0])
 
@@ -292,20 +293,20 @@ class Server:
             if dirname!="/":
                 parent = personal_database.getFile(os.path.dirname(dirname), os.path.basename(dirname))
                 if not parent:
-                    client.send(self.cr.createMessage(b"1Invalid Path", client_public_key, self.private_key, signature_public_key))
+                    client.send(self.cr.createMessage(b"1Invalid Path", client_public_key, self.private_key))
                     client.close()
                     return
                 else:
                     if parent[3]!="DIR":
-                        client.send(self.cr.createMessage(b"1Parent not a directory", client_public_key, self.private_key, signature_public_key))
+                        client.send(self.cr.createMessage(b"1Parent not a directory", client_public_key, self.private_key))
                         client.close()
                         return
 
 
-            size, nonce = self.cr.decryptMessage(client.recv(1024*10), self.private_key, signature_public_key).decode().split()
+            size, nonce = self.cr.decryptMessage(client.recv(1024*10), self.private_key, client_perm_public_key).decode().split()
             size=int(size)
 
-            client.send(self.cr.createMessage(b"0", client_public_key, self.private_key, signature_public_key))
+            client.send(self.cr.createMessage(b"0", client_public_key, self.private_key))
             if type=="FILE":
                 res = self.recvFile(client, path_to_write, int(size))
                 if not res:
@@ -320,7 +321,7 @@ class Server:
 
                 personal_database.addFileToDB(file_id, dirname, basename, metadata)
 
-                client.send(self.cr.createMessage(("\n".join([basename,type, str(size), modification_date, creation_date, compress])).encode(), client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(("\n".join([basename,type, str(size), modification_date, creation_date, compress])).encode(), client_public_key, self.private_key))
 
         elif command[0]=="MKDIR":
             dir_=bytes().fromhex(command[1]).decode()
@@ -329,12 +330,12 @@ class Server:
 
             if dirname!="/":
                 if not personal_database.getFile(os.path.dirname(dirname), os.path.basename(dirname)):
-                    client.send(self.cr.createMessage(b"1Invalid Path.", client_public_key, self.private_key, signature_public_key))
+                    client.send(self.cr.createMessage(b"1Invalid Path.", client_public_key, self.private_key))
                     client.close()
                     return
 
             if personal_database.getFile(dirname, basename):
-                client.send(self.cr.createMessage(b"1Path Already Exists", client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"1Path Already Exists", client_public_key, self.private_key))
                 client.close()
                 return
 
@@ -348,7 +349,7 @@ class Server:
 
                 personal_database.addFileToDB(folder_id, dirname, basename, metadata)
 
-                client.send(self.cr.createMessage(("\n".join(metadata[:-1])).encode(), client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(("\n".join(metadata[:-1])).encode(), client_public_key, self.private_key))
 
             except Exception as e:
                 print(e)
@@ -364,22 +365,22 @@ class Server:
             info = [bytes().fromhex(v) for v in info]
             result = self.H.updateUserPassword(userID, info)
             if result:
-                client.send(self.cr.createMessage(b"0",client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"0",client_public_key, self.private_key))
             else:
-                client.send(self.cr.createMessage(b"1Failed to Change Password",client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"1Failed to Change Password",client_public_key, self.private_key))
 
         elif command[0]=="MOVE":
             item = bytes().fromhex(command[1]).decode()
             destination = bytes().fromhex(command[2]).decode()
 
             if not personal_database.getFile(os.path.dirname(item),os.path.basename(item)):
-                client.send(self.cr.createMessage(b"1File or Folder doesn't exist",client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"1File or Folder doesn't exist",client_public_key, self.private_key))
                 client.close()
                 return
 
             if destination!="/":
                 if not personal_database.getFile(os.path.dirname(destination), os.path.basename(destination)):
-                    client.send(self.cr.createMessage(b"1Destination doesn't exist.",client_public_key, self.private_key, signature_public_key))
+                    client.send(self.cr.createMessage(b"1Destination doesn't exist.",client_public_key, self.private_key))
                     client.close()
                     return
 
@@ -396,12 +397,12 @@ class Server:
             file_info = personal_database.getFile(dirname, basename)
 
             if not file_info:
-                client.send(self.cr.createMessage(b"1Item doesn't exist.", client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"1Item doesn't exist.", client_public_key, self.private_key))
 
             else:
                 self.deletePath(file_to_delete, personal_database, userID)
 
-                client.send(self.cr.createMessage(b"0", client_public_key, self.private_key, signature_public_key))
+                client.send(self.cr.createMessage(b"0", client_public_key, self.private_key))
 
             client.close()
 
@@ -424,7 +425,7 @@ class Server:
                 ans="0"
 
 
-            client.send(self.cr.createMessage(ans.encode(), client_public_key, self.private_key, signature_public_key))
+            client.send(self.cr.createMessage(ans.encode(), client_public_key, self.private_key))
             client.close()
 
 
